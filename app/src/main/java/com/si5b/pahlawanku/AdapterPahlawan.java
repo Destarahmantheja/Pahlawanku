ef12982d0ea49a535a25245883b8b3f51914384a
Desta rahman theja <destarahmantheja@gmail.com>
2022-11-23 11:19:52 +0700

    Project Pahlawanku lanjutan
    menambah image

--- app/src/main/java/com/si5b/pahlawanku/AdapterPahlawan.java
+++ app/src/main/java/com/si5b/pahlawanku/AdapterPahlawan.java
@@ -1,6 +1,7 @@
 package com.si5b.pahlawanku;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -33,6 +36,29 @@ public class AdapterPahlawan extends RecyclerView.Adapter<AdapterPahlawan.VHPahl
         ModelPahlawan pahlawan = dataPahlawan.get(position);
         holder.tvNama.setText(pahlawan.getNama());
         holder.tvTentang.setText(pahlawan.getTentang());
+
+        Glide
+                .with(ctx)
+                .load(pahlawan.getFoto())
+                .centerCrop()
+                .into(holder.ivFoto);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String xNama, xTentang, xFoto;
+
+                xNama = pahlawan.getNama();
+                xTentang = pahlawan.getTentang();
+                xFoto = pahlawan.getFoto();
+
+                Intent kirim = new Intent(ctx, DetailActivity.class);
+                kirim.putExtra("xNama", xNama);
+                kirim.putExtra("xTentang", xTentang);
+                kirim.putExtra("xFoto", xFoto);
+                ctx.startActivity(kirim);
+            }
+        });
     }
 
     @Override
@@ -49,6 +75,7 @@ public class AdapterPahlawan extends RecyclerView.Adapter<AdapterPahlawan.VHPahl
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvTentang = itemView.findViewById(R.id.tv_tentang);
             ivFoto = itemView.findViewById(R.id.iv_foto);
+
 
         }
     }
